e372707aaaca552b903b0ee29239ad749e576c43
NarasaNarayan <139985371+NarasaNarayan@users.noreply.github.com>
2025-04-03 14:26:38 +0530

    Update Sidebar2.jsx

--- frontend/src/Components/Sidebar2/Sidebar2.jsx
+++ frontend/src/Components/Sidebar2/Sidebar2.jsx
@@ -17,7 +17,7 @@ const Sidebar2 = () => {
       if (!token) return;
 
       try {
-        const response = await fetch("http://localhost:5000/api/cart", {
+        const response = await fetch("https://new-kayaka2025-march.onrender.com/api/cart", {
           method: "GET",
           headers: { Authorization: `Bearer ${token}` },
         });
